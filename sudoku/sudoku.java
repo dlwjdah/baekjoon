f9e56f10826080c3668ff89b53bf290afbcf7d0c
leejungmo <thgkftjfk@naver.com>
2020-10-08 17:55:47 +0900

    스도쿠 만들기 ..

---
+++ sudoku/sudoku.java
@@ -0,0 +1,26 @@
+package sudoku;
+
+public class sudoku {
+    public static void main(String[] args) {
+
+        System.out.println("============+===========+===========+");
+        System.out.println("|   |   |   +   |   |   +   |   |   +");
+        System.out.println("------------+-----------+-----------+");
+        System.out.println("|   |   |   +   |   |   +   |   |   +");
+        System.out.println("------------+-----------+-----------+");
+        System.out.println("|   |   |   +   |   |   +   |   |   +");
+        System.out.println("============+===========+===========+");
+        System.out.println("|   |   |   +   |   |   +   |   |   +");
+        System.out.println("------------+-----------+-----------+");
+        System.out.println("|   |   |   +   |   |   +   |   |   +");
+        System.out.println("------------+-----------+-----------+");
+        System.out.println("|   |   |   +   |   |   +   |   |   +");
+        System.out.println("============+===========+===========+");
+        System.out.println("|   |   |   +   |   |   +   |   |   +");
+        System.out.println("------------+-----------+-----------+");
+        System.out.println("|   |   |   +   |   |   +   |   |   +");
+        System.out.println("------------+-----------+-----------+");
+        System.out.println("|   |   |   +   |   |   +   |   |   +");
+        System.out.println("============+===========+===========+");
+    }
+}
